428424c638c9b3afebae786ede3afb1e98e0adf5
Tobias Buschor <tobias.buschor@shwups.ch>
2021-05-24 11:50:19 +0200

    refine

--- mod.js
+++ mod.js
@@ -6,12 +6,11 @@ function run(){
     const hash = location.hash;
     if (!hash.startsWith('#:~:text=')) return;
     const parts = hash.substr(9).split(',');
+    if (parts.length > 1) return console.log('multiple parts not supported');
     for (let part of parts) {
         if (part.startsWith('-')) return console.log('prefix not supported');
         if (part.endsWith('-')) return console.log('suffix not supported');
     }
-    if (parts.length > 1) return console.log('multiple parts not supported');
-
     window.find(parts[0]);
     const selection = getSelection();
     const range = selection.getRangeAt(0);
@@ -24,10 +23,8 @@ function run(){
         while (mark.firstChild) mark.before(mark.firstChild);
         mark.remove();
     }
-    document.addEventListener('mousedown',remove);
-//    document.addEventListener('scroll',remove);
-
+    addEventListener('mousedown',remove);
+    //document.addEventListener('scroll',remove);
 }
 
-
-document.addEventListener('DOMContentLoaded', run);
+addEventListener('DOMContentLoaded', run);
