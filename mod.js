6da52d45e5d3b224dcbef75f04935b01f3716b32
Tobias Buschor <tobias.buschor@shwups.ch>
2022-05-16 14:34:06 +0200

    May 16, 2022 2:34 PM

--- mod.js
+++ mod.js
@@ -3,20 +3,20 @@
 function run(){
     const hash = location.hash;
     if (!hash.startsWith('#:~:text=')) return;
-console.log('run');
     const parts = hash.substring(9).split(',');
     if (parts.length > 1) return console.log('multiple parts not supported');
     for (let part of parts) {
         if (part.startsWith('-')) return console.log('prefix not supported');
         if (part.endsWith('-')) return console.log('suffix not supported');
     }
+    console.log('run');
     window.find(parts[0]);
     const selection = getSelection();
     const range = selection.getRangeAt(0);
     if (!range) return;
     const mark = document.createElement('mark');
     range.surroundContents(mark);
-    selection.collapseToEnd()
+    selection.collapseToEnd();
 
     function remove(){
         while (mark.firstChild) mark.before(mark.firstChild);
